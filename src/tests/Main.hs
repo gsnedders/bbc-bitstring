6894097da1aeffdc018d9463dc0eea9ffef28bc9
Geoffrey Sneddon <geoffers@gmail.com>
2014-07-23 12:37:40 +0100

    Fix tests and the associated cabal file so tests will build.

--- src/tests/Main.hs
+++ src/tests/Main.hs
@@ -1,3 +1,11 @@
 module Main where
 
+import Bitstring.Internal
+
+import Text.Printf
 import Test.QuickCheck
+
+main  = mapM_ (\(s,a) -> printf "%-25s: " s >> a) tests
+
+tests :: [(String, IO ())]
+tests = []
